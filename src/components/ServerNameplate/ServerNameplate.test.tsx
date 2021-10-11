5f189f2205ea90e69b5f98b5cdd2716be12aaed1
James <tjamesperry@hotmail.com>
2021-10-10 23:19:35 -0500

    components/ServerNameplate: refactored test to use LiveServer and DormantServer definitions

--- src/components/ServerNameplate/ServerNameplate.test.tsx
+++ src/components/ServerNameplate/ServerNameplate.test.tsx
@@ -1,8 +1,11 @@
-import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
+import { DateTime } from "luxon";
 
+import { ThemeProvider } from "styled-components";
+import { light } from "themes/light";
 import ServerNameplate from "./ServerNameplate";
+import { DormantServer, LiveServer } from "../../providers/Server/Server";
 
 let container: HTMLElement;
 beforeEach(() => {
@@ -17,23 +20,55 @@ afterEach(() => {
   container.remove();
 });
 
-it("renders", () => {
+it("renders LiveServer", () => {
+  const updated = DateTime.now();
+  const created = DateTime.now().minus({ hours: 1 });
+
+  const server: LiveServer = {
+    __typename: "LiveServer",
+    id: "UUID",
+    associationID: "associationUUID",
+    activePlayers: 99,
+    queuedPlayers: 10,
+    definition: {
+      id: "UUID",
+      instanceID: "instanceUUID",
+      instanceKind: "instanceKind",
+      allocationID: "allocationID",
+      elasticIP: "elasticIP",
+      name: "name",
+      region: "region",
+      description: "description",
+      url: "url",
+      bannerURL: "bannerURL",
+      background: "AIRPORT",
+      tickRate: 30,
+      mapSalt: 1,
+      mapSeed: 2,
+      mapSize: 3000,
+      maxPlayers: 200,
+      mapWipeFrequency: "mapWipeFrequency",
+      blueprintWipeFrequency: "blueprintWipeFrequency",
+      schedule: [],
+      tags: [],
+    },
+    updatedAt: updated,
+    createdAt: created,
+  };
+
   act(() => {
     render(
-      <ServerNameplate
-        name="US East Alpha"
-        elasticIP="192.168.0.1:28016"
-        activePlayers={99}
-        queuedPlayers={10}
-      />,
+      <ThemeProvider theme={light}>
+        <ServerNameplate server={server} />
+      </ThemeProvider>,
       container
     );
   });
   expect(container.querySelector(".server-nameplate__name").textContent).toBe(
-    "US East Alpha"
+    "name"
   );
   expect(container.querySelector(".server-nameplate__ip").textContent).toBe(
-    "192.168.0.1:28016"
+    "elasticIP"
   );
   expect(
     container.querySelector(".server-nameplate__players--value").textContent
@@ -41,4 +76,64 @@ it("renders", () => {
   expect(
     container.querySelector(".server-nameplate__queue--value").textContent
   ).toBe("10");
+  expect(
+    container.querySelector(".server-nameplate__countdown--value").textContent
+  ).toBe("01:00:00");
+});
+
+it("renders DormantServer", () => {
+  const updated = DateTime.now();
+  const created = DateTime.now().minus({ hours: 1 });
+  const starts = DateTime.now().plus({ hours: 1 });
+
+  const server: DormantServer = {
+    __typename: "DormantServer",
+    id: "UUID",
+    definition: {
+      id: "UUID",
+      instanceID: "instanceUUID",
+      instanceKind: "instanceKind",
+      allocationID: "allocationID",
+      elasticIP: "elasticIP",
+      name: "name",
+      region: "region",
+      description: "description",
+      url: "url",
+      bannerURL: "bannerURL",
+      background: "AIRPORT",
+      tickRate: 30,
+      mapSalt: 1,
+      mapSeed: 2,
+      mapSize: 3000,
+      maxPlayers: 200,
+      mapWipeFrequency: "mapWipeFrequency",
+      blueprintWipeFrequency: "blueprintWipeFrequency",
+      schedule: [],
+      tags: [],
+    },
+    startsAt: starts,
+    updatedAt: updated,
+    createdAt: created,
+  };
+
+  act(() => {
+    render(
+      <ThemeProvider theme={light}>
+        <ServerNameplate server={server} />
+      </ThemeProvider>,
+      container
+    );
+  });
+  expect(container).toBeDefined();
+  expect(container.querySelector(".server-nameplate__name").textContent).toBe(
+    "name"
+  );
+  expect(container.querySelector(".server-nameplate__ip")).toBeNull();
+  expect(
+    container.querySelector(".server-nameplate__players--value")
+  ).toBeNull();
+  expect(container.querySelector(".server-nameplate__queue--value")).toBeNull();
+  expect(
+    container.querySelector(".server-nameplate__countdown--value").textContent
+  ).toBe(starts.diffNow().toFormat("hh:mm:ss"));
 });
