6dccbad6b63bea69bf012185ec1444c310163ca0
James <tjamesperry@hotmail.com>
2021-10-10 23:31:05 -0500

    App: moved providers into App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { ThemeProvider } from 'styled-components';
-import { light } from 'themes/light';
-import '@fontsource/oxanium';
-import './index.css';
-
-import { ApolloProvider } from '@apollo/client';
-import client from './apollo/apollo';
-
-import { StylesProvider } from '@material-ui/core/styles';
-
-import { SnackbarProvider } from 'notistack';
-import { UserServiceProvider } from './providers/User/User';
-import { ServerServiceProvider } from './providers/Server/Server';
+import "@fontsource/oxanium";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={light}>
-        <ApolloProvider client={client}>
-          <StylesProvider injectFirst>
-            <SnackbarProvider>
-              <UserServiceProvider>
-                <ServerServiceProvider>
-                  <App />
-                </ServerServiceProvider>
-              </UserServiceProvider>
-            </SnackbarProvider>
-          </StylesProvider>
-        </ApolloProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
