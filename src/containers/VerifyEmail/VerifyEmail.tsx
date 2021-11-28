5519824d918494deb71140654390f49d65004910
James <tjamesperry@hotmail.com>
2021-11-28 15:55:26 -0600

    containers/VerifyEmail: polished VerifyEmail status processing

--- src/containers/VerifyEmail/VerifyEmail.tsx
+++ src/containers/VerifyEmail/VerifyEmail.tsx
@@ -2,6 +2,7 @@ import { LoadingCard } from '../../components/Card/LoadingCard';
 
 import { useEffect, useState } from 'react';
 import { useRouter } from '../../router/router';
+import { toast } from 'react-hot-toast';
 
 import { useVerifyEmail } from '../../services/user/hooks';
 import { VerifyEmailArgs } from '../../services/user/types';
@@ -14,18 +15,35 @@ const VerifyEmail = ({ className }: VerifyEmailProps): JSX.Element => {
   const router = useRouter<VerifyEmailArgs>();
   const verifyEmail = useVerifyEmail();
 
-  const [text, setText] = useState<string>('Verifying your email.');
-  const [status, setStatus] = useState<'loading' | 'success' | 'error'>('success');
+  const [text, setText] = useState<string>('Verifying ...');
+  const [status, setStatus] = useState<'loading' | 'success' | 'error'>('loading');
 
-  // if (!router.query.hash) {
-  //   router.push('/servers');
-  // }
+  if (!router.query.hash) {
+    router.push('/servers');
+  }
 
   useEffect(() => {
     if (typeof router.query.hash === 'string') {
-      verifyEmail.mutate({ hash: router.query.hash });
-      setText('Verified!');
-      setStatus('success');
+      verifyEmail.mutate(
+        { hash: router.query.hash },
+        {
+          onSuccess: () => {
+            setText('Verified! Returning to home.');
+            setStatus('success');
+            setTimeout(() => {
+              toast.success('Email verified!');
+              router.push('/servers');
+            }, 2000);
+          },
+          onError: () => {
+            setText('Unable to verify. Returning to home.');
+            setStatus('error');
+            setTimeout(() => {
+              router.push('/servers');
+            }, 2000);
+          },
+        },
+      );
     }
   }, [router.query.hash]);
 
