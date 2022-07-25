badb88dc2216eac25f3b7fb5a2ccaed1f7c1d8bc
james <tjper@proton.me>
2022-07-24 20:07:20 -0500

    import @fontsource/bebas-neue before importing index.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/bebas-neue';
 import './index.css';
 
 ReactDOM.render(
