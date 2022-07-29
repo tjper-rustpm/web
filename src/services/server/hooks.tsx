54c90e45beef30d1feb0f1f3175e2a9e4bd4cc5a
james <tjper@proton.me>
2022-07-28 23:22:39 -0500

    cache server list via react-query

--- src/services/server/hooks.tsx
+++ src/services/server/hooks.tsx
@@ -4,27 +4,31 @@ import { client } from '../../axios/axios';
 import { AnyServer, Subscriptions } from './types';
 
 export function useServers(): UseQueryResult<AnyServer[], Error> {
-  return useQuery('servers', async ({ signal }) => {
-    const getServers = client.get<AnyServer[]>('/server-api/v1/servers', { signal });
-    const getSubscriptions = client.get<Subscriptions[]>('/payment-api/v1/servers', { signal });
+  return useQuery(
+    'servers',
+    async ({ signal }) => {
+      const getServers = client.get<AnyServer[]>('/server-api/v1/servers', { signal });
+      const getSubscriptions = client.get<Subscriptions[]>('/payment-api/v1/servers', { signal });
 
-    const res = await Promise.all([getServers, getSubscriptions]);
-    const [serversDetails, subscriptionsDetails] = res;
+      const res = await Promise.all([getServers, getSubscriptions]);
+      const [serversDetails, subscriptionsDetails] = res;
 
-    const servers = new Map(
-      serversDetails.data.map((server: AnyServer): [string, AnyServer] => {
-        return [server.id, server];
-      }),
-    );
+      const servers = new Map(
+        serversDetails.data.map((server: AnyServer): [string, AnyServer] => {
+          return [server.id, server];
+        }),
+      );
 
-    subscriptionsDetails.data.forEach((subscriptions: Subscriptions) => {
-      const server = servers.get(subscriptions.id);
-      if (!server) {
-        return;
-      }
-      servers.set(subscriptions.id, { ...server, ...{ subscriptions: subscriptions } });
-    });
+      subscriptionsDetails.data.forEach((subscriptions: Subscriptions) => {
+        const server = servers.get(subscriptions.id);
+        if (!server) {
+          return;
+        }
+        servers.set(subscriptions.id, { ...server, ...{ subscriptions: subscriptions } });
+      });
 
-    return Array.from(servers.values());
-  });
+      return Array.from(servers.values());
+    },
+    { staleTime: 1000 * 60 },
+  );
 }
