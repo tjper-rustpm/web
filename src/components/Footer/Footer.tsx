67c3cc40d820f178d6f53635034b23e4c131d01b
james <tjper@proton.me>
2022-09-24 15:48:06 -0500

    src/components/Footer: updated to show 1337 Perry LLC

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = (): JSX.Element => (
-  <footer className="text-sm max-w-fit mx-auto mt-16">
-    <h4>&copy; Rustpm</h4>
+  <footer className="max-w-fit mx-auto mt-16">
+    <h4 className="text-sm">1337 Perry LLC</h4>
   </footer>
 );
 
