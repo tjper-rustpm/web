91f1897282d3a9cbf5721fa5a4f7e2ee816452f5
james <tjper@proton.me>
2023-01-03 16:04:41 -0600

    src/components/Footer: have footer handle its own margin-top and not Servers component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import logo from '../../imgs/1337PerryLLC.webp';
 
 const Footer = (): JSX.Element => (
-  <footer className="max-w-fit mx-auto mt-16">
+  <footer className="max-w-fit mx-auto mt-32">
     <div className="w-44 p-2">
       <img src={logo} alt="1337 Perry LLC" />
     </div>
