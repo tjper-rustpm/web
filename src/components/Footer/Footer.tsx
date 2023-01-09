132dac2a8bef2033ea601a59f34996379ee1f022
james <tjper@proton.me>
2023-01-09 13:32:45 -0600

    src/components/Footer: removed Perry 1337 logo from footer (unecessary)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,3 @@
-import logo from '../../imgs/1337PerryLLC.webp';
-
-const Footer = (): JSX.Element => (
-  <footer className="max-w-fit mx-auto mt-32">
-    <div className="w-44 p-2">
-      <img src={logo} alt="1337 Perry LLC" />
-    </div>
-  </footer>
-);
+const Footer = (): JSX.Element => <footer className="max-w-fit mx-auto mt-32"></footer>;
 
 export default Footer;
